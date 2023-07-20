aae2d6d20abcc46034af965010e4f02ae9002336
freddysundowner <githumbi3fred@gmail.com>
2023-07-20 20:24:42 +0300

    r2

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3002",
     credentials: true,
     methods: "GET,POST,PUT,DELETE, PATCH",
     maxAge: 3600,
