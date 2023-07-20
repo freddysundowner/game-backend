e721b21f5dde820164fc7fb359b9eb57e69348c2
freddysundowner <githumbi3fred@gmail.com>
2023-07-20 20:23:17 +0300

    r1

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const { Server } = require("socket.io");
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE, PATCH",
+    maxAge: 3600,
+  })
+);
+
 // setup socket.io and register it with the server
 const io = require("socket.io")(server, {
   cors: {
