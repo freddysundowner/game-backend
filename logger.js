0b9695e32f6952b8e5381c8fc1d9579c57cbfafb
Fredrick <githumbi3fred@gmail.com>
2023-09-02 12:06:08 +0300

    added docker

---
+++ logger.js
@@ -0,0 +1,21 @@
+// logger.js
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(({ timestamp, level, message }) => {
+    return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+    level: 'info',
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+module.exports = logger;
